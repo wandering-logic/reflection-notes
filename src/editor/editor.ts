8f028e9c7f75772ba6c46eb3d9980868d7ade5d7
Matt Frank <matthew.i.frank@gmail.com>
2026-01-10 21:35:23 -0600

    make the various formatting changes visible in menus

--- src/editor/editor.ts
+++ src/editor/editor.ts
@@ -1,4 +1,4 @@
-import { baseKeymap, toggleMark } from "prosemirror-commands";
+import { baseKeymap, setBlockType, toggleMark } from "prosemirror-commands";
 import { history, redo, undo } from "prosemirror-history";
 import { keymap } from "prosemirror-keymap";
 import { schema } from "prosemirror-schema-basic";
@@ -26,4 +26,50 @@ export function doUndo(view: EditorView): boolean {
 
 export function doRedo(view: EditorView): boolean {
   return redo(view.state, view.dispatch);
+}
+
+// Block type commands
+export function setParagraph(view: EditorView): boolean {
+  return setBlockType(schema.nodes.paragraph)(view.state, view.dispatch);
+}
+
+export function setHeading(view: EditorView, level: number): boolean {
+  return setBlockType(schema.nodes.heading, { level })(
+    view.state,
+    view.dispatch,
+  );
+}
+
+export function setCodeBlock(view: EditorView): boolean {
+  return setBlockType(schema.nodes.code_block)(view.state, view.dispatch);
+}
+
+export function setBlockquote(view: EditorView): boolean {
+  // Blockquote wraps other blocks, so we need wrapIn instead of setBlockType
+  // For now, this is a simplified version
+  return setBlockType(schema.nodes.paragraph)(view.state, view.dispatch);
+}
+
+export function insertHorizontalRule(view: EditorView): boolean {
+  const { state, dispatch } = view;
+  const hr = schema.nodes.horizontal_rule.create();
+  dispatch(state.tr.replaceSelectionWith(hr));
+  return true;
+}
+
+// Mark commands
+export function toggleStrong(view: EditorView): boolean {
+  return toggleMark(schema.marks.strong)(view.state, view.dispatch);
+}
+
+export function toggleEm(view: EditorView): boolean {
+  return toggleMark(schema.marks.em)(view.state, view.dispatch);
+}
+
+export function toggleCode(view: EditorView): boolean {
+  return toggleMark(schema.marks.code)(view.state, view.dispatch);
+}
+
+export function toggleLink(view: EditorView, href: string): boolean {
+  return toggleMark(schema.marks.link, { href })(view.state, view.dispatch);
 }
--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,47 @@ app.innerHTML = `
   <div class="layout">
     <header class="menubar">
       <div class="menu">File</div>
-      <div class="menu" id="edit-undo">Undo</div>
-      <div class="menu" id="edit-redo">Redo</div>
-      <div class="menu">Edit</div>
+      <div class="menu">
+        Edit
+        <div class="menu-dropdown">
+          <div class="menu-item" id="edit-undo">Undo</div>
+          <div class="menu-item" id="edit-redo">Redo</div>
+        </div>
+      </div>
+      <div class="menu">
+        Format
+        <div class="menu-dropdown">
+          <div class="menu-item has-submenu">
+            Text
+            <div class="submenu">
+              <div class="menu-item" id="format-strong">Strong</div>
+              <div class="menu-item" id="format-em">Emphasis</div>
+              <div class="menu-item" id="format-code">Code</div>
+              <div class="menu-item" id="format-link">Link...</div>
+            </div>
+          </div>
+          <div class="menu-item has-submenu">
+            Paragraph
+            <div class="submenu">
+              <div class="menu-item" id="format-paragraph">Normal Text</div>
+              <div class="menu-item" id="format-blockquote">Block Quote</div>
+              <div class="menu-item" id="format-code-block">Code Block</div>
+              <div class="menu-item" id="format-hr">Horizontal Rule</div>
+              <div class="menu-item has-submenu">
+                Headings
+                <div class="submenu">
+                  <div class="menu-item" id="format-h1">Heading 1</div>
+                  <div class="menu-item" id="format-h2">Heading 2</div>
+                  <div class="menu-item" id="format-h3">Heading 3</div>
+                  <div class="menu-item" id="format-h4">Heading 4</div>
+                  <div class="menu-item" id="format-h5">Heading 5</div>
+                  <div class="menu-item" id="format-h6">Heading 6</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div class="menu">View</div>
       <div class="menu">Preferences</div>
       <div class="menu">Help</div>
@@ -42,5 +80,57 @@ document.querySelector("#edit-undo")?.addEventListener("click", () => {
 
 document.querySelector("#edit-redo")?.addEventListener("click", () => {
   Editor.doRedo(view);
+  view.focus();
+});
+
+// Format menu - block types
+document.querySelector("#format-paragraph")?.addEventListener("click", () => {
+  Editor.setParagraph(view);
+  view.focus();
+});
+
+for (let level = 1; level <= 6; level++) {
+  document.querySelector(`#format-h${level}`)?.addEventListener("click", () => {
+    Editor.setHeading(view, level);
+    view.focus();
+  });
+}
+
+document.querySelector("#format-code-block")?.addEventListener("click", () => {
+  Editor.setCodeBlock(view);
+  view.focus();
+});
+
+document.querySelector("#format-blockquote")?.addEventListener("click", () => {
+  Editor.setBlockquote(view);
+  view.focus();
+});
+
+document.querySelector("#format-hr")?.addEventListener("click", () => {
+  Editor.insertHorizontalRule(view);
+  view.focus();
+});
+
+// Format menu - marks
+document.querySelector("#format-strong")?.addEventListener("click", () => {
+  Editor.toggleStrong(view);
+  view.focus();
+});
+
+document.querySelector("#format-em")?.addEventListener("click", () => {
+  Editor.toggleEm(view);
+  view.focus();
+});
+
+document.querySelector("#format-code")?.addEventListener("click", () => {
+  Editor.toggleCode(view);
+  view.focus();
+});
+
+document.querySelector("#format-link")?.addEventListener("click", () => {
+  const href = prompt("Enter URL:");
+  if (href) {
+    Editor.toggleLink(view, href);
+  }
   view.focus();
 });
